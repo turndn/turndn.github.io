d04b7e1aa371a9020dcf47c1121eac0379c91c39
up <turndn.up@gmail.com>
2018-12-27 20:36:18 +0900

    Add Grate Western Trail Calculator

---
+++ grate.js
@@ -0,0 +1,39 @@
+var scores = new Array();
+var elems = ["#coin", "#building", "#city", "#station", "#hazard", "#cow",
+             "#aim", "#station_master", "#labor", "#board", "#token"];
+
+function push_score(name) {
+  var score = new Vue({
+    el: name,
+    data: {
+      value: 0
+    }
+  })
+  scores.push(score);
+}
+
+for (var i = 0; i < elems.length; i++) {
+  push_score(elems[i]);
+}
+
+var result = new Vue({
+  el: '#result',
+  data: {
+    message: 0
+  }
+})
+
+var calc = new Vue({
+  el: '#calc',
+  methods: {
+    calc: function(event) {
+      var sum = 0;
+      for (var i = 0; i < scores.length; i++) {
+        var score = parseInt(scores[i].value, 10);
+        if (score != NaN)
+          sum += score;
+      }
+      result.message = sum;      
+    }
+  }
+})
