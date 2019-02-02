d0d4bc0425fc4636bc5be5842d87835b5a13e300
up <turndn.up@gmail.com>
2019-02-02 13:02:48 +0900

    Modify to use material design in Grate Calculator

---
+++ grate_material.js
@@ -0,0 +1,24 @@
+
+var unchi = new Vue({
+  el: '#app',
+  data: () => ({
+    drawer: null,
+    message: "unchi",
+    scores: [],
+    result: null
+  }),
+  methods: {
+    run: function(event) {
+      var sum = 0;
+      for (var i = 0; i < this.scores.length; i++) {
+        if (typeof this.scores[i] !== "undefined") {
+          var score = parseInt(this.scores[i], 10);
+          if (!isNaN(score)) {
+            sum += score;
+          }
+        }
+      }
+      this.result = sum;
+    }
+  }
+})
