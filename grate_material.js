7d82c7e8bd25ce54f23b5d0b0aad1cdc63e85f44
up <turndn.up@gmail.com>
2019-02-02 19:16:39 +0900

    Change variable name

--- grate_material.js
+++ grate_material.js
@@ -1,24 +1,21 @@
-
-var unchi = new Vue({
-  el: '#app',
-  data: () => ({
-    drawer: null,
-    message: "unchi",
-    scores: [],
-    result: null
-  }),
-  methods: {
-    run: function(event) {
-      var sum = 0;
-      for (var i = 0; i < this.scores.length; i++) {
-        if (typeof this.scores[i] !== "undefined") {
-          var score = parseInt(this.scores[i], 10);
-          if (!isNaN(score)) {
-            sum += score;
-          }
+new Vue({
+el: '#app',
+data: () => ({
+  drawer: null,
+  scores: [],
+  result: null
+}),
+methods: {
+  run: function(event) {
+    var sum = 0;
+    for (var i = 0; i < this.scores.length; i++) {
+      if (typeof this.scores[i] !== "undefined") {
+        var score = parseInt(this.scores[i], 10);
+        if (!isNaN(score)) {
+          sum += score;
         }
       }
-      this.result = sum;
     }
+    this.result = sum;
   }
-})
+}})
